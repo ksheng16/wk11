ef8e2d71881427413195ee8e5c72d1f26cb42e59
ksheng16 <kgsheng@umich.edu>
2025-11-05 09:21:40 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,55 @@
+var video = document.querySelector("#player1")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("In slower")
+	console.log("Current speed is", video.playbackRate)
+	video.playbackRate *= 0.9;
+
+	console.log("New speed is", video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("In faster")
+	console.log("Current speed is", video.playbackRate)
+	video.playbackRate *= 1.1;
+
+	console.log("New speed is", video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("In skip")
+	console.log("The duration is", video.duration)
+	console.log("Current location is" , video.currentTime)
+
+	video.currentTime += 15
+
+	console.log("New location is", video.currentTime)
+});
+
+document.querySelector("mute").addEventListener("click", function() {
+	if (video.muted) {
+        video.muted = false;
+        muteBtn.textContent = "Mute";
+        console.log("Unmuted");
+    } else {
+        video.muted = true;
+        muteBtn.textContent = "Unmute";
+        console.log("Muted");
+    }
+});
+
+
